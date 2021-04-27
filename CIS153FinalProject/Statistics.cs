a30e75ca41345cdcc851e3dc59b0253ace068ee0
ShaneDubyaa <shane.werlinger@gmail.com>
2021-04-27 09:38:52 -0400

    Write to text file (fail)
    I tried to get the stats to write to the text file but when I do it says the file is in use.

--- CIS153FinalProject/Statistics.cs
+++ CIS153FinalProject/Statistics.cs
@@ -104,6 +104,7 @@ namespace CIS153FinalProject
                 
                 text = text + line + "\n";
                 line = inputfile.ReadLine();
+                inputfile.Close();
 
             }
             playerwins = count1.ToString();
