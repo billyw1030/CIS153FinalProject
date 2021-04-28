96f6d19871764b89152fb94fdb16391b7ff3b002
billyw1030 <billyw1030@gmail.com>
2021-04-28 19:41:27 -0400

    Moved function

--- CIS153FinalProject/Statistics.cs
+++ CIS153FinalProject/Statistics.cs
@@ -64,78 +64,6 @@ namespace CIS153FinalProject
 
         private void Statistics_Load(object sender, EventArgs e)
         {
-            //StreamReader inputfile = new StreamReader("../../Resources/statsfile.txt");
-            //srClosed = false;
-            ////StreamReader inputfile = new StreamReader("C:\\Users\\FleaF\\Documents\\GitHub\\CIS153FinalProject\\CIS153FinalProject\\statsfile.txt.txt");
-            //int count1 = 0;
-            //int count2 = 0;
-            //int count3 = 0;
-            //int count4 = 0;
-            //float playerpercent1 = 0.0F;
-            //float aipercent1 = 0.0F;
-            //string text = " ";
-            //string line;
-            //line = inputfile.ReadLine();
-            //while (line !=null)
-            //{
-                
-            //    if (line.Equals("1"))
-            //    {
-            //        count1++;
-
-            //    }
-
-            //    if (line.Equals("2"))
-            //    {
-            //        count2++;
-
-
-            //    }
-            //    if (line.Equals("3"))
-            //    {
-            //        count3++;
-
-
-            //    }
-            //    if (line.Equals("4"))
-            //    {
-            //        count4++;
-
-
-            //    }
-                
-            //    text = text + line + "\n";
-            //    line = inputfile.ReadLine();
-            //    inputfile.Close();
-            //    srClosed = true;
-
-            //}
-            //playerwins = count1.ToString();
-            //aiwins = count2.ToString();
-            //tie = count3.ToString();
-            //totalplayed = count4.ToString();
-            //playerpercent1 = ((float)count1 / (float)count4) * 100;
-            //aipercent1 = ((float)count2 / (float)count4) * 100;
-            //playerpercent = playerpercent1.ToString("0.0") + "%";
-            //aipercent = aipercent1.ToString("0.0") + "%";
-            //txt_stats.Text = text;
-            //inputfile.Close();
-            //srClosed = true;
-
-            //playerwins = text;
-            //aiwins = text;
-            //tie = text;
-            //totalplayed = text;
-            //playerpercent = text;
-            //aipercent = text;
-            //txt_stats.Text = text;
-            //inputfile.Close();
-
-
-        }
-
-        private void Txt_stats_TextChanged(object sender, EventArgs e)
-        {
             StreamReader inputfile = new StreamReader("../../Resources/statsfile.txt");
             srClosed = false;
             //StreamReader inputfile = new StreamReader("C:\\Users\\FleaF\\Documents\\GitHub\\CIS153FinalProject\\CIS153FinalProject\\statsfile.txt.txt");
@@ -214,8 +142,8 @@ namespace CIS153FinalProject
                     for (int i = 0; i < listofplayerstats.Count(); i++)
                     {
                         txt_stats.Text = "";
-                        txt = txt + "Player Wins: " + listofplayerstats[i].GetPlayerwins() + Environment.NewLine + "Ai wins: " + listofplayerstats[i].GetAiwins() + Environment.NewLine + "Tie: " + listofplayerstats[i].GetTie()
-                            + Environment.NewLine + "Total games played: " + listofplayerstats[i].GetTotalplayed() + Environment.NewLine + "Player percentage: " + listofplayerstats[i].GetPlayerpercent() + Environment.NewLine + "Ai percentage: " + listofplayerstats[i].GetAipercent();
+                        txt = txt + "Player Wins: " + listofplayerstats[i].GetPlayerwins() + Environment.NewLine + "AI wins: " + listofplayerstats[i].GetAiwins() + Environment.NewLine + "Tie: " + listofplayerstats[i].GetTie()
+                            + Environment.NewLine + "Total games played: " + listofplayerstats[i].GetTotalplayed() + Environment.NewLine + "Player percentage: " + listofplayerstats[i].GetPlayerpercent() + Environment.NewLine + "AI percentage: " + listofplayerstats[i].GetAipercent();
 
                         txt_stats.Text = txt;
 
@@ -226,6 +154,22 @@ namespace CIS153FinalProject
                 }
                 sw.Close();
             }
+
+            //playerwins = text;
+            //aiwins = text;
+            //tie = text;
+            //totalplayed = text;
+            //playerpercent = text;
+            //aipercent = text;
+            //txt_stats.Text = text;
+            //inputfile.Close();
+
+
+        }
+
+        private void Txt_stats_TextChanged(object sender, EventArgs e)
+        {
+           
 
 
         }
